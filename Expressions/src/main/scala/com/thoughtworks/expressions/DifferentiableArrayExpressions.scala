3eed24594861bc6d703a33e52100e3b35ef04a91
Yang Bo <pop.atry@gmail.com>
2018-01-09 10:43:47 +0800

    DifferentiableArrayExpressions should extends DifferentiableBooleanExpressions in case of no implementation of BooleanTerm.gradient found

--- Expressions/src/main/scala/com/thoughtworks/expressions/DifferentiableArrayExpressions.scala
+++ Expressions/src/main/scala/com/thoughtworks/expressions/DifferentiableArrayExpressions.scala
@@ -5,13 +5,12 @@ import com.thoughtworks.expressions.Anonymous.Implicitly
 /**
   * @author 杨博 (Yang Bo)
   */
-trait DifferentiableArrayExpressions extends ArrayExpressions with DifferentiableValueExpressions {
+trait DifferentiableArrayExpressions extends ArrayExpressions with DifferentiableBooleanExpressions {
   protected trait ValueTypeApi extends super.ValueTypeApi { this: ValueType =>
 
     protected trait ExtractFromArrayBufferApi extends ValueTermApi with super.ExtractFromArrayBufferApi {
       protected val operand0: ArrayBufferTerm { type ElementTerm = TypedTerm }
-      def gradient(x: ValueTerm)(
-          implicit debuggingInformation: Implicitly[DebuggingInformation]): DeltaTerm = {
+      def gradient(x: ValueTerm)(implicit debuggingInformation: Implicitly[DebuggingInformation]): DeltaTerm = {
         ???
       }
 
