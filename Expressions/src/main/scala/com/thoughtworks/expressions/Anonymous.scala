852fa1bb11f2b320689a621011e3d8ee020040ef
Yang Bo <pop.atry@gmail.com>
2018-01-10 22:22:46 +0800

    Remove opaque type for Implicitly

--- Expressions/src/main/scala/com/thoughtworks/expressions/Anonymous.scala
+++ Expressions/src/main/scala/com/thoughtworks/expressions/Anonymous.scala
@@ -11,31 +11,14 @@ trait Anonymous extends Any
 
 object Anonymous {
 
-  protected sealed trait ImplicitlyCompanion {
-
-    /** Shadow [[Anonymous.implicitValue]] in case of compiler crash when looking for [[scala.languageFeature.higherKinds]] */
-    private def implicitValue(): Unit = {}
-
-    type OpaqueType[+A] <: A with Anonymous
-
-    def apply[A](underlying: A with Anonymous): OpaqueType[A]
-
-  }
-
-  protected val Implicitly: ImplicitlyCompanion = new ImplicitlyCompanion {
-    type OpaqueType[+A] = A with Anonymous
-
-    def apply[A](underlying: A with Anonymous): OpaqueType[A] = underlying
-  }
-
-  type Implicitly[A] = Implicitly.OpaqueType[A]
+  type Implicitly[A] = A with Anonymous
 
   implicit def implicitValue[A, Constructor, ImplicitApplied](
       implicit factory: Factory.Aux[(A with Anonymous), Constructor],
       implicitApply: ImplicitApply.Aux[Constructor, ImplicitApplied],
       asImplicitValue: ImplicitApplied <:< (A with Anonymous)
   ): Implicitly[A] = {
-    Implicitly(asImplicitValue(implicitApply(factory.newInstance)))
+    asImplicitValue(implicitApply(factory.newInstance))
   }
 
 }
