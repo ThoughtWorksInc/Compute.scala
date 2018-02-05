6e8077ff32fe0e6221aa045b6f3ac330627e1929
杨博 (Yang Bo) <pop.atry@gmail.com>
2018-02-05 16:54:11 +0800

    Upgrade AMD-APP-SDK to 3.0

--- .travis/amd_sdk.sh
+++ .travis/amd_sdk.sh
@@ -12,7 +12,8 @@ POSTID_STRING='name="post_id"'
 NONCE2_STRING='name="amd_developer_central_nonce"'
 
 #For newest FORM=`wget -qO - $URL | sed -n '/download-2/,/64-bit/p'`
-FORM=`wget -qO - $URL | sed -n '/download-5/,/64-bit/p'`
+#For older FORM=`wget -qO - $URL | sed -n '/download-5/,/64-bit/p'`
+FORM=`wget -qO - $URL | sed -n '/download-2/,/64-bit/p'`
 
 # Get nonce from form
 NONCE1=`echo $FORM | awk -F ${NONCE1_STRING} '{print $2}'`
