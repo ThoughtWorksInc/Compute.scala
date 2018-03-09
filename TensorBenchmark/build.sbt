60cf79d5ce83f919a35d9d45483788d374fcc9f5
Yang Bo <atryyang@thoughtworks.com>
2018-03-09 15:06:16 +0800

    Add benchmark for convolutional on Tensor

--- TensorBenchmark/build.sbt
+++ TensorBenchmark/build.sbt
@@ -1,1 +1,26 @@
 enablePlugins(JmhPlugin)
+
+libraryDependencies += "org.nd4j" % "nd4j-api" % "0.9.1"
+
+libraryDependencies += "org.nd4j" % "nd4j-native-platform" % "0.9.1"
+
+libraryDependencies += ("org.lwjgl" % "lwjgl" % "3.1.6").jar().classifier {
+  import scala.util.Properties._
+  if (isMac) {
+    "natives-macos"
+  } else if (isLinux) {
+    "natives-linux"
+  } else if (isWin) {
+    "natives-windows"
+  } else {
+    throw new MessageOnlyException(s"lwjgl does not support $osName")
+  }
+}
+
+scalacOptions += "-Ypartial-unification"
+
+sourceDirectory in Jmh := (sourceDirectory in Test).value
+
+classDirectory in Jmh := (classDirectory in Test).value
+
+dependencyClasspath in Jmh := (dependencyClasspath in Test).value
--- TensorBenchmark/src/test/scala/com/thoughtworks/compute/TensorBenchmark.scala
+++ TensorBenchmark/src/test/scala/com/thoughtworks/compute/TensorBenchmark.scala
@@ -1,30 +1,126 @@
 package com.thoughtworks.compute
 
 import com.thoughtworks.feature.Factory
-import com.thoughtworks.raii.asynchronous.Do
+import com.thoughtworks.future._
+import com.thoughtworks.continuation._
+import com.thoughtworks.raii.asynchronous._
+import com.thoughtworks.raii.covariant._
+import com.thoughtworks.tryt.covariant._
 import com.typesafe.scalalogging.StrictLogging
 import org.lwjgl.opencl.CLCapabilities
-import org.openjdk.jmh.annotations.{Scope, State}
+import org.openjdk.jmh.annotations._
+import scalaz.syntax.all._
+import scalaz.std.list._
+
+import scala.util.Try
 
 /**
   * @author 杨博 (Yang Bo)
   */
 @State(Scope.Benchmark)
 class TensorBenchmark {
-  private def doTensors: Do[Tensors] =
-    Do.monadicCloseable(Factory[
-      StrictLogging with OpenCL.LogContextNotification with OpenCL.GlobalExecutionContext with OpenCL.UseAllDevices with OpenCL.UseFirstPlatform with OpenCL.CommandQueuePool with Tensors with OpenCL.DontReleaseEventTooEarly]
-      .newInstance(
-        numberOfCommandQueuesForDevice = { (deviceId: Long, capabilities: CLCapabilities) =>
-          5
-        }
-      ))
+  import com.thoughtworks.compute.TensorBenchmark._
+
+  @Param(Array("5", "1", "2", "10"))
+  private var numberOfLayers: Int = 3
+
+  trait Benchmarks
+      extends StrictLogging
+      with OpenCL.LogContextNotification
+      with OpenCL.GlobalExecutionContext
+      with OpenCL.UseAllDevices
+      with OpenCL.UseFirstPlatform
+      with OpenCL.CommandQueuePool
+      with OpenCL.DontReleaseEventTooEarly
+      with Tensors.WangHashingRandomNumberGenerator
+      with Convolution {
+
+    protected val numberOfCommandQueuesForDevice: (Long, CLCapabilities) => Int = { (_, _) =>
+      30
+    }
+
+    override def monadicClose: UnitContinuation[Unit] = {
+      super.monadicClose
+    }
+
+    def doComputeConvolution(): Do[() => Array[Float]] = {
+      val input = Tensor.random(Array(32, 32, 32, 3))
+      val layers = (for (i <- (0 until numberOfLayers).view) yield {
+        ConvolutionalLayer(weight = Tensor.random(Array(3, 3, 3, 3)), bias = Tensor.random(Array(3)))
+      }).toList
+
+      input.doBuffer.flatMap { _ =>
+        layers
+          .traverseM {
+            case ConvolutionalLayer(weight, bias) =>
+              weight.doBuffer.flatMap { weightBuffer =>
+                bias.doBuffer.map { biasBuffer =>
+                  List(weightBuffer, biasBuffer)
+                }
+              }
+          }
+          .map { _ => () =>
+            layers
+              .foldLeft(input) { (input, layer) =>
+                layer.forward(input)
+              }
+              .flatArray
+              .run
+              .blockingAwait
+
+          }
+      }
+    }
+
+  }
+
+  private var benchmarkResouce: Resource[UnitContinuation, Try[() => Array[Float]]] = _
+
+  @Setup
+  def setup(): Unit = {
+    assert(benchmarkResouce == null)
+    val Do(TryT(ResourceT(resourceContinuation))) =
+      Do.monadicCloseable(Factory[Benchmarks].newInstance()).flatMap(_.doComputeConvolution())
+    benchmarkResouce = resourceContinuation.blockingAwait()
+  }
+
+  @TearDown(Level.Trial)
+  def tearDown(): Unit = {
+    val benchmarkResouce = this.benchmarkResouce
+    this.benchmarkResouce = null
+    benchmarkResouce.release.blockingAwait
+  }
+
+  @Benchmark
+  def computeConvolution(): Array[Float] = {
+    benchmarkResouce.value.get.apply()
+  }
 
 }
 
 object TensorBenchmark {
 
+  def main(args: Array[String]): Unit = {
+    val tensorBenchMark = new TensorBenchmark
+    tensorBenchMark.setup()
+    try {
+      tensorBenchMark.computeConvolution()
+    } finally {
+      tensorBenchMark.tearDown()
+    }
+//    val Do(TryT(ResourceT(resourceContinuation))) =
+//      Do.monadicCloseable(Factory[Benchmarks].newInstance()).flatMap(_.doComputeConvolution())
+//    benchmarkResouce = resourceContinuation.blockingAwait()
+
+  }
+
   trait Convolution extends Tensors {
+    final case class ConvolutionalLayer(weight: Tensor, bias: Tensor) {
+      def forward(input: Tensor): Tensor = {
+        convolute(input, weight, bias)
+      }
+    }
+
     def convolute(input: Tensor /* batchSize × height × width × depth */,
                   weight: Tensor /* kernelHeight × kernelWidth × depth × filterSize */,
                   bias: Tensor /* filterSize */ ): Tensor = {
@@ -107,7 +203,7 @@ object TensorBenchmark {
                         biasPerFilter.broadcast(Array(batchSize, height, width)) + summands.reduce(_ + _)
                     }
 
-                  zip(outputChannels)
+                  Tensor.zip(outputChannels)
                 case _ =>
                   throw new IllegalArgumentException
               }
