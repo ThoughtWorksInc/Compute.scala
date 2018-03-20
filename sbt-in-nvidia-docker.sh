29f3b2b307501a21be4d6daa51bfdd74126ae64c
杨博 (Yang Bo) <pop.atry@gmail.com>
2018-03-20 15:32:37 +0800

    Update and rename sbt-in-nvidia-docker-1.sh to sbt-in-nvidia-docker.sh

--- sbt-in-nvidia-docker.sh
+++ sbt-in-nvidia-docker.sh
@@ -4,5 +4,5 @@ nvidia-docker run --tty --interactive --init \
   --volume "$HOME/.sbt/boot:/root/.sbt/boot" \
   --volume "$PWD:/mnt/project-root" \
   --workdir /mnt/project-root \
-  popatry/scala-cuda:sbt-openjdk8-cuda9.0-cudnn7-runtime-ubuntu16.04 \
+  popatry/scala-cuda:sbt-openjdk8-cuda8.0-opencl-ubuntu16.04 \
   sbt
