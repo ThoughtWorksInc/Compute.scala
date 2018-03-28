fa09c4af2066e77459d057722bae12208943a5d1
Yang Bo <atryyang@thoughtworks.com>
2018-03-28 16:30:13 +0800

    Don't use Factory as cpu and gpu are simple objects

--- cpu/src/main/scala/com/thoughtworks/compute/cpu.scala
+++ cpu/src/main/scala/com/thoughtworks/compute/cpu.scala
@@ -4,10 +4,19 @@ import com.thoughtworks.feature.Factory
 import com.typesafe.scalalogging.StrictLogging
 import org.lwjgl.opencl.CL10.CL_DEVICE_TYPE_CPU
 
-object cpu {
+object cpu
+    extends StrictLogging
+    with Tensors.UnsafeMathOptimizations
+    with Tensors.SuppressWarnings
+    with OpenCL.LogContextNotification
+    with OpenCL.GlobalExecutionContext
+    with OpenCL.CommandQueuePool
+    with OpenCL.UseAllCpuDevices
+    with OpenCL.DontReleaseEventTooEarly
+    with OpenCL.SynchronizedCreatingKernel
+    with OpenCL.HandleEventInExecutionContext
+    with Tensors.WangHashingRandomNumberGenerator {
 
-  val tensors = Factory[
-    StrictLogging with Tensors.UnsafeMathOptimizations with Tensors.SuppressWarnings with OpenCL.LogContextNotification with OpenCL.GlobalExecutionContext with OpenCL.CommandQueuePool with OpenCL.UseAllCpuDevices with OpenCL.DontReleaseEventTooEarly with OpenCL.SynchronizedCreatingKernel with OpenCL.HandleEventInExecutionContext with Tensors.WangHashingRandomNumberGenerator]
-    .newInstance(numberOfCommandQueuesPerDevice = 5)
+  protected val numberOfCommandQueuesPerDevice = 5
 
 }
--- gpu/src/main/scala/com/thoughtworks/compute/gpu.scala
+++ gpu/src/main/scala/com/thoughtworks/compute/gpu.scala
@@ -4,10 +4,17 @@ import com.thoughtworks.feature.Factory
 import com.typesafe.scalalogging.StrictLogging
 import org.lwjgl.opencl.CL10.CL_DEVICE_TYPE_CPU
 
-object gpu {
-
-  val tensors = Factory[
-    StrictLogging with Tensors.UnsafeMathOptimizations with Tensors.SuppressWarnings with OpenCL.LogContextNotification with OpenCL.GlobalExecutionContext with OpenCL.CommandQueuePool with OpenCL.UseAllGpuDevices with OpenCL.DontReleaseEventTooEarly with OpenCL.SynchronizedCreatingKernel with OpenCL.HandleEventInExecutionContext with Tensors.WangHashingRandomNumberGenerator]
-    .newInstance(numberOfCommandQueuesPerDevice = 5)
-
+object gpu
+    extends StrictLogging
+    with Tensors.UnsafeMathOptimizations
+    with Tensors.SuppressWarnings
+    with OpenCL.LogContextNotification
+    with OpenCL.GlobalExecutionContext
+    with OpenCL.CommandQueuePool
+    with OpenCL.UseAllGpuDevices
+    with OpenCL.DontReleaseEventTooEarly
+    with OpenCL.SynchronizedCreatingKernel
+    with OpenCL.HandleEventInExecutionContext
+    with Tensors.WangHashingRandomNumberGenerator {
+  protected val numberOfCommandQueuesPerDevice: Int = 5
 }
